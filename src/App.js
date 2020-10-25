cfe9272df98420b3044981af4bf605a1ecfe4b8c
unknown <sankara.tanguturi@gmail.com>
2020-10-25 04:16:23 +0000

    added buildspec.yaml

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+	  This is Sankar's first react project!
         </p>
         <a
           className="App-link"
